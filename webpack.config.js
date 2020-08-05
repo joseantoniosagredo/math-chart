f199a6e1bb0a10ff775d9f9ed6470f94fbb7dbd7
Jose Antonio GS <jose.gutierrez.sagredo@gmail.com>
2020-08-05 16:02:36 +0200

    add configuration

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  mode: "development",
+  devtool: "inline-source-map",
+  entry: {
+    main: "./src/index.ts",
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: "[name]-bundle.js",
+  },
+  resolve: {
+    // Add ".ts" and ".tsx" as resolvable extensions.
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+      { test: /\.tsx?$/, loader: "ts-loader" },
+    ],
+  },
+  devServer:{
+    watchContentBase: true,
+    port: 9000,
+    contentBase: path.join(__dirname, 'test'),
+  }
+};
